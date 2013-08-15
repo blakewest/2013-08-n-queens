4167e098a5e2d0de8e98df84450585fa1cfbfefe
Hack Reactor Students <github@hackreactor.com>
2013-08-14 22:04:41 -0700

    Naive solution working for n-rooks

--- spec/solversSpec.js
+++ spec/solversSpec.js
@@ -48,10 +48,10 @@ describe("solvers", function() {
   });
 
   it('should return some boards', function() {
-    var board = window.makeBoard(3);
+    var board = window.makeBoard(6);
     var result = window.rookTraverse(board);
     console.log(result);
-    expect(result.length).to.be.equal(2);
+    expect(result.length).to.be.equal(120);
   });
 
 });
